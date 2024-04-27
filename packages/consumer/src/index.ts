484534105cdd906a88feb5e55640266fe73b9985
Luiz Ferraz <luiz@lferraz.com>
2024-04-27 15:24:11 -0300

    Implement consumer

--- packages/consumer/src/index.ts
+++ packages/consumer/src/index.ts
@@ -1,10 +1,30 @@
-import { defineIntegration } from 'astro-integration-kit';
+import { defineIntegration, withPlugins } from 'astro-integration-kit';
+import sitemap from '@demo/sitemap';
 import { z } from 'astro/zod';
 
 export default defineIntegration({
   name: '@demo/consumer',
   optionsSchema: z.never().optional(),
-  setup() {
-    // TODO: Implement this
+  setup({ name }) {
+    return withPlugins({
+      name,
+      plugins: [sitemap.asPlugin('sitemapApi')],
+      hooks: {
+        'astro:config:setup': ({ sitemapApi, injectRoute, config }) => {
+          injectRoute({
+            pattern: '/added-route',
+            entrypoint: '@demo/consumer/entripoint',
+            prerender: false,
+          });
+
+          // Using AIK the API is available as a plugin
+          sitemapApi?.addExtraPage('/added-route');
+
+          // Get the API without using AIK
+          const noAikApi = sitemap.fromConfig(config);
+          noAikApi?.addExtraPage('/added-route');
+        },
+      }
+    });
   },
 });
